1743b851ee3c27944c24486c756aa53b95aae49e
Jake Jarvis <jake@jarv.is>
2019-09-25 11:14:25 -0400

    add unshaky cask

---
+++ Casks/unshaky.rb
@@ -0,0 +1,18 @@
+cask 'unshaky' do
+  version '0.5.6'
+  sha256 '8d0ed13d2b02e93b067dfa43121abcaaeb16ae6638e54f14e38cc99ff62a295b'
+
+  url "https://github.com/aahung/Unshaky/releases/download/v#{version}/Unshaky_v#{version}.zip"
+  appcast 'https://github.com/aahung/Unshaky/releases.atom'
+  name 'Unshaky'
+  homepage 'https://github.com/aahung/Unshaky'
+
+  depends_on macos: '>= :sierra'
+
+  app 'Unshaky.app'
+
+  zap trash: [
+               "~/Library/Preferences/com.nestederror.Unshaky.plist",
+               "~/Library/Caches/com.nestederror.Unshaky"
+             ]
+end
